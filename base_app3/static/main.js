7e7b393a4d31e42ec2364a12faca87818b5939ce
WorkRobotics <dev@work-robotics.co.jp>
2024-06-12 14:14:27 +0900

    最初のコミット

---
+++ base_app3/static/main.js
@@ -0,0 +1,4 @@
+function clickButton() {
+  const message_element = document.getElementById("message");
+  message_element.innerText = "ボタンがクリックされました。";
+}
---
+++ image_app2/static/main.js
@@ -0,0 +1,9 @@
+// バナナの画像を表示する
+const changeBanana = () => {
+  document.getElementById("imagebox").src = "static/banana.png";
+};
+
+// ラズパイの画像を表示する
+const changeRaspi = () => {
+  document.getElementById("imagebox").src = "static/raspi.png";
+};
